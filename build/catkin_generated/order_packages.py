008392729e24c6eba073898475434faa9bf77e3b
steinsxin <steinsxin@gmail.com>
2024-08-01 16:00:31 +0800

    cv_bridge
    ROS的cv_bridge包重编译

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/steins_xin/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/steins_xin/ROS_test/devel;/home/steins_xin/vision_2023/devel;/home/steins_xin/ROS/devel;/opt/ros/noetic'.split(';') if '/home/steins_xin/ROS_test/devel;/home/steins_xin/vision_2023/devel;/home/steins_xin/ROS/devel;/opt/ros/noetic' != '' else []
---
+++ build/cv_bridge/src/CMakeFiles/cv_bridge_boost.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/steins_xin/catkin_ws/src/cv_bridge/src/module.cpp" "/home/steins_xin/catkin_ws/build/cv_bridge/src/CMakeFiles/cv_bridge_boost.dir/module.cpp.o"
+  "/home/steins_xin/catkin_ws/src/cv_bridge/src/module_opencv.cpp" "/home/steins_xin/catkin_ws/build/cv_bridge/src/CMakeFiles/cv_bridge_boost.dir/module_opencv.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_PYTHON_DYN_LINK"
+  "PYTHON3"
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"cv_bridge\""
+  "cv_bridge_boost_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/steins_xin/catkin_ws/src/cv_bridge/include"
+  "/opt/ros/noetic/include"
+  "/home/steins_xin/catkin_ws/src/cv_bridge/src/."
+  "/usr/include/python3.8"
+  "/usr/lib/python3/dist-packages/numpy/core/include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/steins_xin/catkin_ws/build/cv_bridge/src/CMakeFiles/cv_bridge.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
